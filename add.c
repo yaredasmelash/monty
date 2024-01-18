bb96f22eebb0f2656cf62ea8d70afbdb3f338825
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-18 12:25:22 +0300

    add

--- add.c
+++ add.c
@@ -9,7 +9,6 @@
 void _add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-
 	if (!stack || !*stack || !((*stack)->next))
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
@@ -17,7 +16,6 @@ void _add(stack_t **stack, unsigned int line_number)
 
 		exit(EXIT_FAILURE);
 	}
-
 	temp = *stack;
 	*stack = (*stack)->next;
 	(*stack)->n += temp->n;
